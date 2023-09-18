84791e35fe837526add24131307aa12fa88bae44
Tural Hasanov <tural_hasanov@bk.ru>
2023-09-18 14:25:34 +0400

    update

---
+++ jwt_core_project_camp/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using jwt_core_project_camp.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jwt_core_project_camp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Pager1()
+        {
+            return Ok("Sehife 1 girish ugurlu");
+        }
+    }
+}
